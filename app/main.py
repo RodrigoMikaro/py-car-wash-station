9c366a752d6545299f2b0b445f8b3398bd6784d4
Rodrigo Mikaro <rodrigomikaro@outlook.com.br>
2026-01-31 17:04:37 -0300

    Methods calculate_washing_price and serve_cars added to CarWashStation and completed

--- app/main.py
+++ app/main.py
@@ -11,3 +11,21 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car):
+        cost = car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city_center)
+        return round(cost)
+
+    def serve_cars(self, cars):
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+        return income
+
+    def wash_single_car(self):
+        pass
+
+    def rate_service(self):
+        pass
+
